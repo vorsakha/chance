cc5a6f3baa22a65677e91373de80473919ae5e1f
vorsakha <andreitf.dev@gmail.com>
2021-08-03 19:34:24 -0300

    common/container - adding mobile first

--- components/common/container.tsx
+++ components/common/container.tsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin-left: 250px;
-  padding-left: 1rem;
-  width: calc(1000px - 250px);
+  padding: 0 1rem;
+  width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (min-width: 768px) {
+    margin-left: 250px;
+    width: calc(1000px - 250px);
+  }
 `;
 
 export default Container;
