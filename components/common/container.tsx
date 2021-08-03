280911aa77ba9ab141d4f95206ea1213c999ce91
vorsakha <andreitf.dev@gmail.com>
2021-08-03 15:14:51 -0300

    common/container - container component

---
+++ components/common/container.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-left: 250px;
+  padding-left: 1rem;
+  width: calc(1000px - 250px);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Container;
