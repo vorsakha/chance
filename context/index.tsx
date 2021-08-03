8de43ea4360c107e8d83a002a851e12c630ab74f
vorsakha <andreitf.dev@gmail.com>
2021-08-03 12:03:12 -0300

    typing context

--- context/index.tsx
+++ context/index.tsx
@@ -1,17 +1,46 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  SetStateAction,
+  Dispatch,
+} from "react";
 
-const IndexContext = createContext<any>(undefined);
+interface IndexTypes {
+  index: number;
+  setIndex: Dispatch<SetStateAction<number>>;
+}
+interface DataTypes {
+  contextData:
+    | {
+        clubs: any;
+        odds: any;
+      }[]
+    | null;
+  setData: Dispatch<SetStateAction<any>>;
+}
+
+const IndexContext = createContext<IndexTypes>({} as IndexTypes);
+const DataContext = createContext<DataTypes>({} as DataTypes);
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const [index, setIndex] = useState(0);
+  const [contextData, setData] = useState(null);
 
   return (
     <IndexContext.Provider value={{ index, setIndex }}>
-      {children}
+      <DataContext.Provider value={{ contextData, setData }}>
+        {children}
+      </DataContext.Provider>
     </IndexContext.Provider>
   );
 }
 
 export function useIndex() {
   return useContext(IndexContext);
+}
+
+export function useData() {
+  return useContext(DataContext);
 }
